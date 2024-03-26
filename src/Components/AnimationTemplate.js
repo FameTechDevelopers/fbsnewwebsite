0b1655a32d3e10af7e4f9e9139b5d55922df53db
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-03-26 03:08:16 -0900

    new bug fixed

--- src/Components/AnimationTemplate.js
+++ src/Components/AnimationTemplate.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import { FaDotCircle } from "react-icons/fa";
-import animation from "../assets/Services/animation.JPG"
+import animation from "../assets/Services/animation.jpg"
 import Image from "next/image";
 
 const AnimationTemplate = () => {
   return (
     <div className=" grid grid-cols-12 mt-12">
       <div className=" col-span-8 pr-20">
-        <h2 className=" text-[26px] font-[500] mt-12 mb-5">2D / 3D Animation</h2>
+        <h2 className=" text-[26px] font-[500] mt-12 mb-5">
+          2D / 3D Animation
+        </h2>
         <p className=" text-[20px] font-[500]">
           Our App development package includes all; Requirement Analysis,
           Designing, Development, customer support, and detailed Documentation.
@@ -15,19 +17,27 @@ const AnimationTemplate = () => {
         <div className=" flex flex-wrap justify-between pt-5">
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Requirement Analysis through a questionnaire</p>
+            <p className=" text-[15px]">
+              Requirement Analysis through a questionnaire
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Detailed Level of Efforts Estimate (for transparent costing)</p>
+            <p className=" text-[15px]">
+              Detailed Level of Efforts Estimate (for transparent costing)
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Prototype of App Design with Admin panel for KPI measurement</p>
+            <p className=" text-[15px]">
+              Prototype of App Design with Admin panel for KPI measurement
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Testing, Technical and milestone deliveries documents</p>
+            <p className=" text-[15px]">
+              Testing, Technical and milestone deliveries documents
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
@@ -36,10 +46,10 @@ const AnimationTemplate = () => {
         </div>
       </div>
       <div className="col-span-4">
-      <Image className=" w-[90%] ml-auto rounded-2xl" src={animation} />
+        <Image className=" w-[90%] ml-auto rounded-2xl" src={animation} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AnimationTemplate
+export default AnimationTemplate;
--- src/Components/DigitalMarketingTemplate.js
+++ src/Components/DigitalMarketingTemplate.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import { FaDotCircle } from "react-icons/fa";
-// import digitalMarketing from "../assets/Services/digital-marketing.JPG"
-// import Image from "next/image";
+import digitalMarketing from "../assets/Services/digital-marketing.jpg"
+import Image from "next/image";
 
 const DigitalMarketingTemplate = () => {
   return (
@@ -15,19 +15,27 @@ const DigitalMarketingTemplate = () => {
         <div className=" flex flex-wrap justify-between pt-5">
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Requirement Analysis through a questionnaire</p>
+            <p className=" text-[15px]">
+              Requirement Analysis through a questionnaire
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Detailed Level of Efforts Estimate (for transparent costing)</p>
+            <p className=" text-[15px]">
+              Detailed Level of Efforts Estimate (for transparent costing)
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Prototype of App Design with Admin panel for KPI measurement</p>
+            <p className=" text-[15px]">
+              Prototype of App Design with Admin panel for KPI measurement
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
-            <p className=" text-[15px]">Testing, Technical and milestone deliveries documents</p>
+            <p className=" text-[15px]">
+              Testing, Technical and milestone deliveries documents
+            </p>
           </div>
           <div className="flex items-center my-2">
             <FaDotCircle className=" mr-3" />
@@ -36,11 +44,10 @@ const DigitalMarketingTemplate = () => {
         </div>
       </div>
       <div className="col-span-4">
-        Under work
-      {/* <Image className=" w-[90%] ml-auto rounded-2xl" src={digitalMarketing} /> */}
+        <Image className=" w-[90%] ml-auto rounded-2xl" src={digitalMarketing} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DigitalMarketingTemplate
+export default DigitalMarketingTemplate;
